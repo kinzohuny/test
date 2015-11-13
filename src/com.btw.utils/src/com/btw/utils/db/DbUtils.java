6ddfbc704ceb6648f7f1f86e06445efb33deefef
kinzohuny <kinzo@qq.com>
2015-11-13 19:16:52 +0800

    modify DbUtils's some method implement.

--- src/com.btw.utils/src/com/btw/utils/db/DbUtils.java
+++ src/com.btw.utils/src/com/btw/utils/db/DbUtils.java
@@ -2,7 +2,6 @@ package com.btw.utils.db;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +10,7 @@ import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.apache.commons.dbutils.handlers.MapListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.apache.log4j.Logger;
 
 
@@ -21,12 +21,8 @@ public class DbUtils {
 	private static ConnectionPool connectionPool;
 	private static QueryRunner queryRunner = new QueryRunner();
 	
-//	private final static String driverClassName = "com.mysql.jdbc.Driver";
-	private final static String driverClassName = "net.sf.log4jdbc.DriverSpy";
-	
-//	private final static String url = "jdbc:log4jdbc:mysql://conn0633f3a5.mysql.rds.aliyuncs.com:3306/my_db?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&rewriteBatchedStatements=true";
-//	private final static String username = "usr0633f3a5";
-//	private final static String password = "poincoo123_abc";
+	private final static String driverClassName = "com.mysql.jdbc.Driver";
+//	private final static String driverClassName = "net.sf.log4jdbc.DriverSpy";
 	
 	private final static String url = "jdbc:log4jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&rewriteBatchedStatements=true";
 	private final static String username = "test";
@@ -50,6 +46,22 @@ public class DbUtils {
 	}
 	
 	public static <T> T queryOne(Class<T> clazz, String sql, Object... paras) throws SQLException{
+		Connection connection = getConnect();
+		T result;
+		
+		try {
+			result = queryRunner.query(connection, sql, new ScalarHandler<T>(), paras);
+		} catch (SQLException e) {
+			logger.error(e.getMessage());
+			throw e;
+		} finally {
+			returnConnect(connection);
+		}
+		
+		return result;
+	}
+	
+	public static <T> T queryObject(Class<T> clazz, String sql, Object... paras) throws SQLException{
 		Connection connection = getConnect();
 		T result;
 		
@@ -110,76 +122,6 @@ public class DbUtils {
 			returnConnect(connection);
 		}
 		
-		return result;
-	}
-	
-	public static int updateBatch(String sql,Object[][] paras) throws SQLException{
-		Connection connection = getConnect();
-		int result = 0;
-		
-		try {
-			int[] resultArr = queryRunner.batch(connection, sql, paras);
-			for (int i = 0; i < resultArr.length; i++) {
-				result += resultArr[i];
-			}
-			returnConnect(connection);
-		} catch (SQLException e) {
-			logger.error(e.getMessage());
-			throw e;
-		} finally {
-			returnConnect(connection);
-		}
-		return result;
-	}
-	
-	public static int executeUpdate(String sql,Object... paras) throws SQLException{
-		int result = 0;
-		Connection connection = getConnect();
-		PreparedStatement pstmt = null;
-		try {
-			pstmt = connection.prepareStatement(sql);
-			for(int i = 0;i<paras.length;i++){
-				pstmt.setObject(i+1, paras[i]);
-			}
-			logger.debug(pstmt.toString());
-			result = pstmt.executeUpdate();
-		} catch (SQLException e) {
-			logger.error(pstmt.toString());
-			throw e;
-		} finally {
-			pstmt.close();
-			returnConnect(connection);
-		}
-		return result;
-	}
-	
-	public static int executeBatch(String sql,Object[][] paras) throws SQLException{
-		int result = 0;
-		Connection connection = getConnect();
-		PreparedStatement pstmt = null;
-		try {
-			pstmt = connection.prepareStatement(sql);
-			int index = 1;
-			for(Object[] item : paras){
-				index = 1;
-				for(Object field : item){
-					pstmt.setObject(index++, field);
-				}
-				pstmt.addBatch();
-			}
-			logger.debug(pstmt.toString());
-			int[] resultArr = pstmt.executeBatch();
-			for (int i = 0; i < resultArr.length; i++) {
-				result += resultArr[i];
-			}
-			returnConnect(connection);
-		} catch (SQLException e) {
-			logger.error(pstmt.toString());
-			throw e;
-		} finally {
-			pstmt.close();
-			returnConnect(connection);
-		}
 		return result;
 	}
 	
