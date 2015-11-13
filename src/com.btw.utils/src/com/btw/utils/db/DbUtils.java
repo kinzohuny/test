21dc8b1dab4fc8603c970435203774e941cf9ed8
kinzohuny <kinzo@qq.com>
2015-11-13 18:35:08 +0800

    add db utils

---
+++ src/com.btw.utils/src/com/btw/utils/db/DbUtils.java
@@ -0,0 +1,224 @@
+package com.btw.utils.db;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
+import org.apache.log4j.Logger;
+
+
+public class DbUtils {
+	
+	private final static Logger logger = Logger.getLogger(DbUtils.class);
+	
+	private static ConnectionPool connectionPool;
+	private static QueryRunner queryRunner = new QueryRunner();
+	
+//	private final static String driverClassName = "com.mysql.jdbc.Driver";
+	private final static String driverClassName = "net.sf.log4jdbc.DriverSpy";
+	
+//	private final static String url = "jdbc:log4jdbc:mysql://conn0633f3a5.mysql.rds.aliyuncs.com:3306/my_db?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&rewriteBatchedStatements=true";
+//	private final static String username = "usr0633f3a5";
+//	private final static String password = "poincoo123_abc";
+	
+	private final static String url = "jdbc:log4jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&rewriteBatchedStatements=true";
+	private final static String username = "test";
+	private final static String password = "test";
+	
+	private final static int initConn = 1;
+	private final static int incConn = 1;
+	private final static int maxConn = 10;
+	
+	public static void initDatabase() throws IOException, SQLException {
+
+		logger.info("database init start...");
+		Connection connect = getConnect();
+		if(connect!=null){
+			returnConnect(connect);
+			logger.info("database init success!");
+		}else{
+			throw new RuntimeException("can not connect to "+url);
+		}
+
+	}
+	
+	public static <T> T queryOne(Class<T> clazz, String sql, Object... paras) throws SQLException{
+		Connection connection = getConnect();
+		T result;
+		
+		try {
+			result = queryRunner.query(connection, sql, new BeanHandler<T>(clazz), paras);
+		} catch (SQLException e) {
+			logger.error(e.getMessage());
+			throw e;
+		} finally {
+			returnConnect(connection);
+		}
+		
+		return result;
+	}
+	
+	public static <T> List<T> queryList(Class<T> clazz, String sql, Object... paras) throws SQLException{
+		Connection connection = getConnect();
+		List<T> resultList;
+		
+		try {
+			resultList = queryRunner.query(connection, sql, new BeanListHandler<T>(clazz), paras);
+		} catch (SQLException e) {
+			logger.error(e.getMessage());
+			throw e;
+		} finally {
+			returnConnect(connection);
+		}
+		
+		return resultList;
+	}
+	
+	public static List<Map<String, Object>> queryMapList(String sql, Object... paras) throws SQLException{
+		Connection connection = getConnect();
+		List<Map<String, Object>> resultList;
+		
+		try {
+			resultList = queryRunner.query(connection, sql, new MapListHandler(), paras);
+		} catch (SQLException e) {
+			logger.error(e.getMessage());
+			throw e;
+		} finally {
+			returnConnect(connection);
+		}
+		
+		return resultList;
+	}
+	
+	public static int update(String sql, Object... paras) throws SQLException{
+		Connection connection = getConnect();
+		int result = 0;
+		
+		try {
+			result = queryRunner.update(connection, sql, paras);
+		} catch (SQLException e) {
+			logger.error(e.getMessage());
+			throw e;
+		} finally {
+			returnConnect(connection);
+		}
+		
+		return result;
+	}
+	
+	public static int updateBatch(String sql,Object[][] paras) throws SQLException{
+		Connection connection = getConnect();
+		int result = 0;
+		
+		try {
+			int[] resultArr = queryRunner.batch(connection, sql, paras);
+			for (int i = 0; i < resultArr.length; i++) {
+				result += resultArr[i];
+			}
+			returnConnect(connection);
+		} catch (SQLException e) {
+			logger.error(e.getMessage());
+			throw e;
+		} finally {
+			returnConnect(connection);
+		}
+		return result;
+	}
+	
+	public static int executeUpdate(String sql,Object... paras) throws SQLException{
+		int result = 0;
+		Connection connection = getConnect();
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = connection.prepareStatement(sql);
+			for(int i = 0;i<paras.length;i++){
+				pstmt.setObject(i+1, paras[i]);
+			}
+			logger.debug(pstmt.toString());
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			logger.error(pstmt.toString());
+			throw e;
+		} finally {
+			pstmt.close();
+			returnConnect(connection);
+		}
+		return result;
+	}
+	
+	public static int executeBatch(String sql,Object[][] paras) throws SQLException{
+		int result = 0;
+		Connection connection = getConnect();
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = connection.prepareStatement(sql);
+			int index = 1;
+			for(Object[] item : paras){
+				index = 1;
+				for(Object field : item){
+					pstmt.setObject(index++, field);
+				}
+				pstmt.addBatch();
+			}
+			logger.debug(pstmt.toString());
+			int[] resultArr = pstmt.executeBatch();
+			for (int i = 0; i < resultArr.length; i++) {
+				result += resultArr[i];
+			}
+			returnConnect(connection);
+		} catch (SQLException e) {
+			logger.error(pstmt.toString());
+			throw e;
+		} finally {
+			pstmt.close();
+			returnConnect(connection);
+		}
+		return result;
+	}
+	
+	private synchronized static void returnConnect(Connection connection) {
+		if(connectionPool==null){
+			connectionPool = new ConnectionPool(driverClassName, url, username, password);
+			connectionPool.setTestTable("dual");
+			if(initConn>0&&incConn>0&&maxConn>0&&maxConn>initConn){
+				connectionPool.setInitialConnections(initConn);
+				connectionPool.setIncrementalConnections(incConn);;
+				connectionPool.setMaxConnections(maxConn);;
+			}
+			try {
+				connectionPool.createPool();
+			} catch (Exception e) {
+				logger.fatal("can not create connection pool!system will exit..", e);
+				System.exit(1);
+			}
+		} 
+		connectionPool.returnConnection(connection);;
+	}
+	
+	private synchronized static Connection getConnect() throws SQLException {
+		if(connectionPool==null){
+			connectionPool = new ConnectionPool(driverClassName, url, username, password);
+			connectionPool.setTestTable("dual");
+			if(initConn>0&&incConn>0&&maxConn>0&&maxConn>initConn){
+				connectionPool.setInitialConnections(initConn);
+				connectionPool.setIncrementalConnections(incConn);;
+				connectionPool.setMaxConnections(maxConn);;
+			}
+			try {
+				connectionPool.createPool();
+			} catch (Exception e) {
+				logger.fatal("can not create connection pool!system will exit..", e);
+				System.exit(1);
+			}
+		} 
+		return connectionPool.getConnection();
+	}
+	
+}
